b62c8472206aeed293b6ea83fa7bd9908f7911d9
damegame88 <damem@colostate.edu>
2021-11-16 14:44:41 -0700

    13.1 11/16 add page components, routing

---
+++ src/components/EJuice.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>EJuice</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import EJuiceDetail from "./components/EJuiceDetail.vue";
+import EJuice from "./components/EJuice.vue";
+import NotFound from "./components/NotFound.vue";
+import ReviewCreate from "./components/ReviewCreate.vue";
+import Signup from "./components/Signup.vue";
+import Account from "./components/Account.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/account", component: Account },
+    { path: "/login", component: Login },
+    { path: "/ejuice", component: EJuice },
+    {
+      path: "/ejuice/:pk",
+      component: EJuiceDetail,
+      children: [{ path: "review", component: ReviewCreate }],
+    },
+    { path: "/signup", component: Signup },
+    { path: "/:invalidroute(.*)", component: NotFound },
+  ],
+});
+
+export default router;
