483919cbcf7cdf2bf639188fc66cb7f65a05c742
damegame88 <damem@colostate.edu>
2021-12-07 13:18:38 -0700

    fixed the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>Home</h1>
+    <div class="my-container">
+      <img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/Bombay_logo_21_final_new_540x.jpg?v=1630430224"
+      /><img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/New_store_photo_3_540x.jpg?v=1637019063"
+      /><img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/New_Store_Photo_1_540x.jpg?v=1637018879"
+      /><img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/Bombay_logo_21_final_new_540x.jpg?v=1630430224"
+      /><img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/Bombay_logo_21_final_new_540x.jpg?v=1630430224"
+      /><img
+        src="https://cdn.shopify.com/s/files/1/0105/9314/0836/files/New_store_photo_2_540x.jpg?v=1637018981"
+      />
+    </div>
   </div>
 </template>
 
